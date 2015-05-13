29fe9c741e2eec6d0ccb89ae30ce7ee9b9fbec4a
zachdyer <zach.dyer@zachdyerdesign.com>
2015-05-12 20:27:02 -0500

    Testing another terminal package with atom

--- hello.go
+++ hello.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Greetings, fellow gopher. I made a change and now I'm going to test it on github.")
-}
+	fmt.Println("Adding another line to test an additional package.")
+	}
